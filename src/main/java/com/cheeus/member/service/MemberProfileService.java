b242e872114d2e5c7dc8ca1bd31f7a5ceb935e74
kmssss <rlaalstkd177@gmail.com>
2024-07-16 14:21:47 +0900

    Member Profile and Popularity EDIT

--- src/main/java/com/cheeus/member/service/MemberProfileService.java
+++ src/main/java/com/cheeus/member/service/MemberProfileService.java
@@ -3,6 +3,7 @@ package com.cheeus.member.service;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 
+import com.cheeus.member.domain.MemberPopularity;
 import com.cheeus.member.domain.MemberProfile;
 import com.cheeus.member.exception.MemberException;
 import com.cheeus.member.repository.MemberProfileDao;
@@ -30,16 +31,92 @@ public class MemberProfileService {
 		
 		Integer existNickname = profileDao.existNickname(nickname);
 		
+		if (existNickname > 0) {
+			throw new MemberException("이미 존재하는 닉네임 입니다.", HttpStatus.BAD_REQUEST);
+		}
+		
 		return HttpStatus.OK;
 	}
+	
 	
 	// 회원 정보 불러오기
 	public MemberProfile findByEmail (String email) {
 		
 		MemberProfile findMember = profileDao.findByEmail(email);
-		System.out.println(findMember==null);
 		
 		return findMember;
 	}
+	
+	
+	// 회원 수정
+	public MemberProfile updateMember (MemberProfile membeProfile) {
+		
+		profileDao.updateMember(membeProfile);
+		
+		return membeProfile;
+	}
+	
+	
+	// 회원 탈퇴
+	public HttpStatus deleteMember (String email) {
+		
+		MemberProfile findMember = findByEmail(email);
+		
+		if (findMember == null) {
+			throw new MemberException("존재하지 않은 아이디입니다.", HttpStatus.BAD_REQUEST);
+		}
+		
+		profileDao.DeleteMember(email);
+		
+		return HttpStatus.OK;
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	// 좋아요 개수 불러오기
+	public Integer findPopularity (String email) {
+		
+		return profileDao.countPopularity(email);
+	}
+	
+	
+	// 내가 좋아요 눌렀는가
+	public boolean isClickedPopularity (MemberPopularity popularity) {
+		
+		//liker는 현재 로그인 유저, 나.
+		if (popularity.getLiker() == null) {
+			throw new MemberException("로그인 해주세요.", HttpStatus.BAD_REQUEST);
+		} else {
+			
+			Integer check = profileDao.existPopularity(popularity);
+			
+			if (check == 0) {
+				// 좋아요 안눌름
+				return false;
+			}
+			
+			// 눌름
+			return true;
+		}
+	}
+	
+	// 좋아요 추가/삭제
+	public void addPopularity (MemberPopularity popularity) {
+		
+		if (!isClickedPopularity(popularity)) {
+			// 좋아요가 없으면 좋아요 추가
+			profileDao.addPopularity(popularity);
+			
+		} else {
+			// 있으면 좋아요 삭제
+			profileDao.deletePopularity(popularity);
+		}
+	}
+	
 
 }
