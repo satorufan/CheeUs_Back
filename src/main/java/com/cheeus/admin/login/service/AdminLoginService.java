1a19acc41dda9124d6ad9000b3d36a74f84a6f6a
sys <ssys0729@gmail.com>
2024-08-09 14:40:50 +0900

    admin login db 연결

--- src/main/java/com/cheeus/admin/login/service/AdminLoginService.java
+++ src/main/java/com/cheeus/admin/login/service/AdminLoginService.java
@@ -15,20 +15,12 @@ public class AdminLoginService {
     @Autowired
     private AdminLoginMapper adminMapper;
 
-    private static final String SECRET_KEY = "your_secret_key";  // JWT 서명에 사용될 비밀키
-
-    public String login(String id, String password) throws Exception {
-        AdminLogin admin = adminMapper.findById(id);
+    public AdminLogin login(String id, String password) throws Exception {
+        AdminLogin admin = adminMapper.findAdminById(id);
         if (admin != null && admin.getPassword().equals(password)) {
-            // JWT 토큰 생성
-            return Jwts.builder()
-                    .setSubject(id)
-                    .setIssuedAt(new Date())
-                    .setExpiration(new Date(System.currentTimeMillis() + 86400000)) // 1일 만료 시간 설정
-                    .signWith(SignatureAlgorithm.HS256, SECRET_KEY)
-                    .compact();
-        } else {
-            throw new Exception("잘못된 자격 증명");
+            return admin;
         }
+
+        return null;
     }
 }
